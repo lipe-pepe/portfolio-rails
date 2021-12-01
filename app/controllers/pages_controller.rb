d3cd76d341876ec1be2648e741769be8239cc60c
lipe-pepe <felipepepe21@gmail.com>
2021-12-01 11:55:20 -0300

    First page sketch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
 
     def welcome
-        
+
     end
+
+    def portfolio
+    end
+
+    def about
+
+    end
+
+    def contact
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   root 'pages#welcome'
 
+  get 'about' => 'pages#about'
+
+  get 'portfolio' => 'pages#portfolio'
+
+  get 'contact' => 'pages#contact'
+  
 end
